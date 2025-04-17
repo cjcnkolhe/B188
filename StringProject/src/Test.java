bbc3c2c3c0ec916d635ceb62e4e340335c4ab090
cjcnkolhe <cjcnkolhe@gmail.com>
2025-04-17 12:43:17 +0530

    add stringproject

---
+++ StringProject/src/Test.java
@@ -0,0 +1,81 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String s="  complete java classess  ";
+		
+//		   boolean value = s.contains("z");
+//	   System.out.println(value);
+		
+//		        String newValue  =s.replace("o", "a");
+//		     System.out.println(newValue);
+//		
+		//System.out.println(   s.replace("java","python"));
+		
+		
+		
+		           // System.out.println(s.charAt(9));
+		
+	//	System.out.println(s.length());
+		
+		             //    System.out.println(s.indexOf("java"));//9
+		
+		//System.out.println(s.lastIndexOf("t"));
+		
+	//	System.out.println(s.isEmpty());//false
+		
+//		System.out.println(s);
+//		System.out.println(s.trim());
+		
+//		     String s2[] = s.split(" ");
+//		     System.out.println(s2);
+//		       for (String name : s2) {
+//				System.out.println(name);
+//			}
+//		     
+		
+//		String s2[]=s.split(",");
+//		
+//		for (String n : s2) {
+//			System.out.println(n);
+//		}
+//		
+//		// a=1  b=2  c=3
+//		String a="a";
+//		String b="a";
+//		        // 1)both match=0
+//		        // 2)a>b   return positive number
+//		        //3)a<b     retun negative number 
+//		System.out.println(a.compareTo(b));
+//		
+////		//if(a>b)
+////		{
+////			return 1;
+////		} if(a<b)
+////		{
+////			-1
+////		}
+////		else if(a==b)
+////		{
+////			0
+////		}
+//		     
+//		
+		
+//		String s5="cjc";
+//		char c[]=  s5.toCharArray();
+		
+		       String s2="CjC";
+		       System.out.println(s2.toLowerCase());
+		       System.out.println(s2.toUpperCase());
+		       
+		       
+		
+		
+//		System.out.println(s5);
+//		System.out.println(c[0]);
+		
+	}
+
+}
